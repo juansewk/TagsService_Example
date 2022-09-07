b86ab5cd4927b72d8b086322ee8c7b3dbe05e92d
Sebastián Cruz <jsebastianwrc@gmail.com>
2022-09-06 22:43:27 -0500

    Add project files.

---
+++ TagsService/Controllers/TagsServiceController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TagsService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TagsServiceController : Controller
+    {
+        
+
+        [HttpGet]
+        public IActionResult GetStatusService()
+        {
+            return Ok("ALGO ESTÁ BIEN");
+        }
+
+    
+
+
+
+
+    }
+}
