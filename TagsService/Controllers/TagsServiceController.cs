dfe75efdd6cb6bfc87e3d8dd0bb53bdd3a2f62f2
Sebastián Cruz <jsebastianwrc@gmail.com>
2022-09-18 06:34:38 -0500

    data access layer structure and main methods v1

--- TagsService/Controllers/TagsServiceController.cs
+++ TagsService/Controllers/TagsServiceController.cs
@@ -5,21 +5,50 @@ using TagsService.Models;
 namespace TagsService.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v1/tags")]
     public class TagsServiceController : Controller
     {
-        
-
         [HttpGet]
+        [Route("GetStatus")]
         public IActionResult GetStatusService()
         {
-            TagModel tagModel = new TagModel();
-
-            return Ok("ALGO ESTÁ BIEN");
-
+            return Ok("OK. :)");
         }
 
-    
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok("Get all tags OK.");
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public IActionResult GetById()
+        {
+            return Ok("Get by id OK.");
+        }
+
+        [HttpPost] 
+        [Route("Add")]
+        public IActionResult Add()
+        {
+            return Ok("Add OK.");
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public IActionResult Update()
+        {
+            return Ok("Udate OK.");
+        }
+
+        [HttpPut]
+        [Route("EnableOrDisable")]
+        public IActionResult EnableOrDisable()
+        {
+            return Ok("Enable or disable OK.");
+        }
 
 
 
