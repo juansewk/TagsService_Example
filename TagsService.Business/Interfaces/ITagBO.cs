e3d7cc73d8debafe8ee6406358cd71c15749df39
Sebastián Cruz <jsebastianwrc@gmail.com>
2022-09-20 23:11:33 -0500

    add business layer

---
+++ TagsService.Business/Interfaces/ITagBO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsService.Business.Interfaces
+{
+    internal class ITagBO
+    {
+        
+
+    }
+}
