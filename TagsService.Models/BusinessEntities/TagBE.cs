45a5b1e6a5f2e617d5fb0d15e1efa51d013d08ba
Sebastián Cruz <jsebastianwrc@gmail.com>
2022-09-21 02:13:41 -0500

    data models & business entities

---
+++ TagsService.Models/BusinessEntities/TagBE.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsService.Models.BusinessEntities
+{
+    public class TagBE
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+
+        [MaxLength(250)]
+        public string? Description { get; set; }
+
+        [MaxLength(10)]
+        public string? HexColor { get; set; }
+
+        public string? Image { get; set; }
+
+        [MaxLength(1)]
+        public bool? IsActive { get; set; }
+    }
+}
---
+++ TagsService.Models/DataModels/TagModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TagsService.Models.DataModels
+{
+    public class TagModel
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+
+        [MaxLength(250)]
+        public string? Description { get; set; }
+
+        [MaxLength(10)]
+        public string? HexColor { get; set; }
+
+        public string? Image { get; set; }
+
+        [MaxLength(1)]
+        public bool? IsActive { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
