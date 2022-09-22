62922b27be3bbc9b6d9b5c0ea3fd745759ded1e6
Sebastián Cruz <jsebastianwrc@gmail.com>
2022-09-21 19:36:29 -0500

    migration can null

--- TagsService.DataContext/Migrations/20220922003346_InnitDB.Designer.cs
+++ TagsService.DataContext/Migrations/20220922003346_InnitDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TagsService.DataContext.Context;
 
@@ -11,9 +12,10 @@ using TagsService.DataContext.Context;
 namespace TagsService.DataContext.Migrations
 {
     [DbContext(typeof(TagsServiceContext))]
-    partial class TagsServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20220922003346_InnitDB")]
+    partial class InnitDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
